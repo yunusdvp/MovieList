ea7568de1c849873ee947230ff2ca0554c2de1e4
Başak Altuntaş <basakaltuntas@gmail.com>
2023-08-21 12:25:03 +0300

    added MovieViewModel and MovieListView

---
+++ MovieList/ViewModel/FilmViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  FilmViewModel.swift
+//  MovieList
+//
+//  Created by Başak on 21.08.2023.
+//
+
+import Foundation
+import SwiftUI
+
+class MovieListeViewModel: ObservableObject {
+    
+    @Published var movies = [Movie]()
+    
+    let downloaderClient = DownloaderClient()
+    
+    func searchMovie(movieName: String) {
+        downloaderClient.downloadMovie(search: movieName) { (result) in
+            switch result {
+                case.failure(let error):
+                    print(error)
+            case.success(let movieArray):
+                if let filmArray = movieArray {
+                    self.movies = movieArray
+                }
+            }
+        }
+    }
+    
+}
